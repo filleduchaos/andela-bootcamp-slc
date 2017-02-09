a25470d78b4957d7fe3a88d3698998d487416470
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-09 10:05:04 +0100

    Implement non-numbers

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -2,7 +2,11 @@
 
 module.exports = {
 	fizzBuzz: function(num) {
-		if(num%15 === 0) {
+		if (typeof num !== 'number') {
+			return '';
+		}
+		
+		if (num%15 === 0) {
 			return 'FizzBuzz';
 		} else if (num%3 === 0) {
 			return 'Fizz';
