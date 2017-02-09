eaba896934f6d2ff83d40bd80515431484168134
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-09 10:31:45 +0100

    Implement case for empty array and fix spec syntax

--- aritgeo/spec/aritgeo-spec.js
+++ aritgeo/spec/aritgeo-spec.js
@@ -8,7 +8,7 @@
     describe("Case for an empty array", function() {
 
       it("should return 0 for an empty array", function() {
-        expect(aritGeo([])).toEqual(0);
+        expect(app.aritGeo([])).toEqual(0);
       });
 
     });
@@ -16,15 +16,15 @@
     describe("Case for an arithmetic sequence", function() {
 
       it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
-        expect(aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
+        expect(app.aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
-        expect(aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
+        expect(app.aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
       });
 
       it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
-        expect(aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
+        expect(app.aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
       });
 
     });
@@ -32,15 +32,15 @@
     describe("Case for a geometric sequence", function() {
 
       it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
-        expect(aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
+        expect(app.aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
-        expect(aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
+        expect(app.aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
       });
 
       it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
-        expect(aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
+        expect(app.aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
       });
 
     });
@@ -48,23 +48,23 @@
     describe("Case for neither arithmetic nor geometric sequence", function() {
 
       it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        expect(aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
+        expect(app.aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
       });
 
       it("should return -1 for [1, 3, 6, 10, 15]", function() {
-        expect(aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
+        expect(app.aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
       });
 
       it("should return -1 for [1, 8, 27, 64, 125]", function() {
-        expect(aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
+        expect(app.aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
       });
 
       it("should return -1 for [19]", function() {
-        expect(aritGeo([19])).toEqual(-1);
+        expect(app.aritGeo([19])).toEqual(-1);
       });
 
       it("should return -1 for [15, 16]", function() {
-        expect(aritGeo([15, 16])).toEqual(-1);
+        expect(app.aritGeo([15, 16])).toEqual(-1);
       });
       
     });
@@ -72,7 +72,7 @@
     describe("Case for both arithmetic and geometric sequence", function() {
 
       it("should return `Arithmetic and Geometric` for [1, 1, 1, 1]", function() {
-        expect(aritGeo([1, 1, 1, 1])).toEqual('Arithmetic and Geometric');
+        expect(app.aritGeo([1, 1, 1, 1])).toEqual('Arithmetic and Geometric');
       });
 
     });
@@ -80,11 +80,11 @@
     describe("Case for non-array argument", function() {
 
       it("should return null for `andela`", function() {
-        expect(aritGeo('andela')).toBeNull();
+        expect(app.aritGeo('andela')).toBeNull();
       });
 
       it("should return null for 128", function() {
-        expect(aritGeo(128)).toBeNull();
+        expect(app.aritGeo(128)).toBeNull();
       });
       
     });
---
+++ aritgeo/src/aritgeo.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+	aritGeo: function(numlist) {
+		if (numlist.length === 0) {
+			return 0;
+		}
+	}
+}
