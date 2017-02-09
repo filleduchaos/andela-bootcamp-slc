e34a94f1e27d3fb1cc44556d42c5e7447ee5ae23
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-09 10:08:09 +0100

    Complete implementation of fizzBuzz

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,11 +1,17 @@
 'use strict'
 
 module.exports = {
+	// This function implements the Fizzbuzz test
 	fizzBuzz: function(num) {
+		// Return empty string if num is not a number
 		if (typeof num !== 'number') {
 			return '';
 		}
-		
+
+		/* Return FizzBuzz if num is a multiple of 3 and 5
+			Return Fizz if num is a multiple of 3 only
+			Return Buzz if num is a multiple of 5 only
+		*/
 		if (num%15 === 0) {
 			return 'FizzBuzz';
 		} else if (num%3 === 0) {
