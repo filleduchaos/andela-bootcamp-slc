63b38bdb6b304d495ed17ae2defa1f7512491c5c
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-09 11:02:53 +0100

    Implement case for arithmetic sequences

--- aritgeo/src/aritgeo.js
+++ aritgeo/src/aritgeo.js
@@ -13,5 +13,19 @@ module.exports = {
 		if (numlist.length === 1 || numlist.length === 2) {
 			return -1;
 		}
+
+		if (module.compute.isArithmetic(numlist.slice(1), numlist[1] - numlist[0])) {
+			return 'Arithmetic';
+		}
+	}
+}
+
+module.compute = {
+	isArithmetic: function(numlist, diff) {
+		if (numlist[1] - numlist[0] === diff) {
+			if (numlist.length === 2) {
+				return true;
+			} else return true && this.isArithmetic(numlist.slice(1), numlist[1] - numlist[0]);	
+		} else return false;
 	}
 }
