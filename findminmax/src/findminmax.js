5e583c08d4588ca4ac2c8babc5362b06e1fc5a65
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-09 03:52:03 +0100

    Implement findMinMax functionality for empty array.

--- findminmax/src/findminmax.js
+++ findminmax/src/findminmax.js
@@ -3,6 +3,10 @@
 module.exports = {
 	// This function finds the minimum and maximum values in an array of numbers.
 	findMinMax: function(numlist) {
+		if (numlist.length === 0) {
+			return [];
+		}
+		
 		numlist.sort( function (a,b) {return a - b} );
 
 		if (numlist[0] == numlist[numlist.length - 1]) {
